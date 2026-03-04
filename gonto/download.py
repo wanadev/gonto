e37682f09e0e56f21951b6271052e4491afc86bd
Fabien LOISON <flo@flogisoft.com>
2026-03-04 09:47:53 +0100

    feat(download): Implemented HTTP download function

---
+++ gonto/download.py
@@ -0,0 +1,51 @@
+import urllib.request
+from pathlib import Path
+from collections.abc import Callable
+
+
+def http_get(
+    url: str,
+    destination: str | Path,
+    allow_overwrite: bool = False,
+    callback: Callable[[float], None] | None = None,
+) -> None:
+    """Downdoad a file using HTTP protocol.
+
+    :param url: The URL of the file to download.
+    :param destination: The path of the destination file.
+    :param allow_overwrite: Allow to overwrite existing files (default:
+        ``False``).
+    :param callback: A callable to track download progress (default: ``None``).
+
+        Callback definition::
+
+            def callback(progress: float) -> None:
+                pass
+
+    :raises FileExistsError: If the output file already exists and
+        ``allow_overwrite`` is False.
+    """
+    output_file_path = Path(destination)
+    output_file_path_partial = output_file_path.with_suffix(".part")
+
+    if output_file_path.is_file() and not allow_overwrite:
+        raise FileExistsError(
+            "The output file already exists: %s" % str(output_file_path)
+        )
+
+    if not output_file_path_partial.parent.is_dir():
+        output_file_path_partial.parent.mkdir(parents=True)
+
+    response = urllib.request.urlopen(url)
+    downloaded_length = 0
+    total_length = int(response.getheader("Content-Length") or 0)
+
+    with open(output_file_path_partial, "wb") as output_file:
+        while chunk := response.read(1024 * 1024):
+            output_file.write(chunk)
+            downloaded_length += len(chunk)
+            progress = downloaded_length / total_length if total_length else 0
+            if callback:
+                callback(progress)
+
+    output_file_path_partial.rename(output_file_path)
